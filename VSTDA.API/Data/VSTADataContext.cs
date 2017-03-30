a2f9ecc97c0591f8869cdfd5c3b30ad46ab29c00
Andrij Wilhitecd <andrij.wilhite@gmail.com>
2017-03-30 13:16:26 -0700

    initial commit

---
+++ VSTDA.API/Data/VSTADataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using VSTDA.API.Models;
+
+namespace VSTDA.API.Data
+{
+    public class VSTADataContext : DbContext
+    {
+        public VSTADataContext() : base("VSTDA")
+        {
+
+        }
+        public IDbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ VSTDA.API/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VSTDA.API.Models
+{
+    public class TodoItem
+    {
+        public int TodoItemID { get; set; }
+        public string Text { get; set; }
+        public string Priority { get; set; }
+    }
+}
